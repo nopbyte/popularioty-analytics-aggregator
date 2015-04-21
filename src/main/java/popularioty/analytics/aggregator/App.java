5979dc18b1686555e4d2a95ab11a55b75917ab1c
nopbyte <juandabytes@gmail.com>
2015-04-21 18:20:48 +0200

    skeleton

---
+++ src/main/java/popularioty/analytics/aggregator/App.java
@@ -0,0 +1,13 @@
+package popularioty.analytics.aggregator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
